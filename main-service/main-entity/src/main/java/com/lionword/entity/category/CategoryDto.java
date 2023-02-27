b03f5701a300c9c81ce83d6ca3c94175a4cf0675
LionWord <PanVlVl@yandex.ru>
2023-02-27 16:09:11 +0300

    map entities to db

--- main-service/main-entity/src/main/java/com/lionword/entity/category/CategoryDto.java
+++ main-service/main-entity/src/main/java/com/lionword/entity/category/CategoryDto.java
@@ -1,11 +1,14 @@
 package com.lionword.entity.category;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "categories")
 public class CategoryDto {
     @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "name")
     private String name;
 }
