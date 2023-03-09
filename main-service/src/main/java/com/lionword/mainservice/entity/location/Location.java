762f97ad9e76824803731527ce43789f27c9ba2b
LionWord <PanVlVl@yandex.ru>
2023-03-09 15:07:48 +0300

    feature: now you can add EventFullDto

--- main-service/src/main/java/com/lionword/mainservice/entity/location/Location.java
+++ main-service/src/main/java/com/lionword/mainservice/entity/location/Location.java
@@ -1,22 +1,30 @@
 package com.lionword.mainservice.entity.location;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.lionword.mainservice.entity.event.EventFullDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "locations")
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class Location {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private long id;
     @Column(name = "event_id")
+ //   @JsonIgnore
     private long eventId;
     @Column(name = "latitude")
     private float lat;
     @Column(name = "longitude")
     private float lon;
-    @OneToOne(mappedBy = "location")
-    private EventFullDto event;
+
 }
